a56759f369c57626b7b9813871757a08d33de245
nchg4 <noamglick@yahoo.com>
2024-06-09 16:52:55 +0300

    finished

--- Week 2/Day 1/Daily Challenge/Daily.Challenge_Wk2Day1.py
+++ Week 2/Day 1/Daily Challenge/Daily.Challenge_Wk2Day1.py
@@ -1,20 +1,15 @@
 # Ask user for a string
-text =input("Enter a string (10 characters long): ")
+text =(input("Enter a string (10 characters long): "))
 
 # Check the length of the string
-if len(text) < 10:
+if  len (text) < 10:
     print ("String not long enough")
 elif len(text) > 10:
     print ("String too long")
 else:
     print ("Perfect string")
 
-# Print the first and last characters of the string
-print ("First character:"), text[0]
-print ("Last character:"), text[-1]
 
-# Print the string character by character using a for loop
-print ("Printing string character by character:")
-for char in text:
-    print ("char")
+
+
 
