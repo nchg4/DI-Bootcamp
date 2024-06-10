96b46ccfdf45b6cefbd51bd5a35528332641727f
nchg4 <noamglick@yahoo.com>
2024-06-10 17:08:28 +0300

    resubmit

--- Week 2/Day 1/excercise.1_Wk2.Day1.py
+++ Week 2/Day 1/excercise.1_Wk2.Day1.py
@@ -1,28 +1,23 @@
-#Hello world x 4
+#Hello World x 4; Exercise 1:
 for _ in range (4):
     print("Hello World")
 
-#Do the equation 
-print(99**3)*8
+#Math; Exercise 2:
+print(99**3 * 8)
 
-#follow the steps:
-# 5 < 3
-# 3 == 3
-# 3 == "3"
-# "3" > 3
-# "Hello" == "hello"
-print (5 < 3)
-print (3 == 3)
-print (3 == "3")
-print("3" > 3)
-print ("Hello" == 'hello')
+# #Exercise 3
+# (5 < 3) #False
+# (3 == 3) #True
+# (3 == "3") #False
+# # ("3" > 3)  #Error
+# ("Hello" == 'hello') #False
 
-#My Computer 
+#My computer; Exercise 4:
 computer = "MacBook Air"
 info = "I have a {}" .format(computer)
 print(info)
 
-#About me
+#My info.; Exercise 5:
 name = "Noam"
 age = 22
 shoe_size = 43
@@ -30,9 +25,7 @@ value = "I grew up in Miami. I am an Israeli, French and US citizen"
 info = "Hello my name is %s I am %d years old, %s. I am size %d shoes" % (name, age, value, shoe_size)
 print(info)
 
-# Create two variables, a and b.
-# Each variable value should be a number.
-# If a is bigger then b, have your code print Hello World.
+#A,B; Exercise 6:
 a = 3000
 b = 10000
 if a > b:
@@ -42,34 +35,24 @@ if b > a:
 if a <= b:
     print("Error")
 
- # Asking user for a number
+ #Even/Odd; Exercise 7:
 number = int(input("Enter any number:"))
-# Check the number if its odd or even
 if number % 2 == 0:
     print("number is even.")
 else:
     print("number is odd.")
 
+#What is your name; Exercise 8:
+user_name =(input("Your Name:"))
+my_name = "Noam"
+if user_name == my_name:
+    print("We have the same name!!")
+else:
+    print("Not my name but...Welcome!")
 
-# Are you tall enough to ride the ride?
-    # Ask the user for their height in cm
+#Height; Exercise 9:
 height_cm = float(input("What is your height in centimeters?: "))
-# Check if the user is tall enough to ride
 if height_cm > 145:
     print("You are tall enough to ride!")
 else:
     print("Sorry, you need to grow some more to ride.")
-
-
-# Ask the user what their name is
-user_name =(input("Your Name:"))
-
-my_name = "Noam"
-
-# Check if the user's name is the same as mine
-if user_name == my_name:
-    print("Wow, we have the same name!?")
-else:
-    print("Welcome!")
-
-    
