5693bb54ab79c7a8a2c7dee3222e5ed5d40849ea
nchg4 <noamglick@yahoo.com>
2024-06-13 22:03:04 +0300

    px done

--- Week 2/Day 3/Exercises/px.py
+++ Week 2/Day 3/Exercises/px.py
@@ -4,7 +4,7 @@ values = [10, 20, 30]
 dict = {keys: values for keys, values in zip(keys, values)}
 print(dict)
 
-#  Cinemax #2; Exercise 2:
+# Cinemax #2; Exercise 2:
 family = {"Rick": 43, 'Beth': 13, 'Morty': 5, 'Summer': 8}
 
 ticket = {
@@ -25,6 +25,8 @@ for name, age in family.items():
 print(f"Total cost for the family: {total_cost}")
 
 # Zara; Exercise 3:
+major_color = {
+    "France": "Blue", 'Spain': 'red', 'US': 'pink' 'green'}
 brand = {
     "name": "Zara",
     "creation_date": 1975,
@@ -32,12 +34,54 @@ brand = {
     "type_of_clothes": ["men", "women", "children", "home"],
     "international_competitors": ["Gap", "H&M", "Benetton"],
     "number_stores": 7000,
-    "major_color": {"France": ["blue"], "Spain": ["red"], "US": ["pink", "green"]},
-}
+    "major_color": major_color}
+
 brand["number_stores"]=2
-#4
+print("Zaras clients include 'types_of_clothes")
 brand["country creation"] = "Spain"
 print(brand)
-# if "international_competitors" in brand:
+if 'international_compeditors' in brand:
+    brand['international_competitors'].append("Desigual")
 
-# print(brand)
+del brand["creation_date"]
+
+print(brand["international_competitors"][-1])
+
+print('Major clothing store in the US:', brand["major_color"]["US"])
+
+print('Number of key-value paris:', len(brand))
+
+print("Keys of the dictionary:", list(brand.keys()))
+
+more_about_zara = {
+    'creation_date': 1975,
+    'number_stores': 10000}
+
+brand.update(more_about_zara)
+
+print("Numer of stores updatep:", brand["number_stores"])
+
+# Exercise 4 : Disney Characters
+users = ["Mickey","Minnie","Donald","Ariel","Pluto"]
+
+#1
+users_A = {}
+
+for index, name in enumerate(users):
+    users_A[name] = index
+
+    print(users_A)
+
+# 2
+    users_B = {}
+
+for index, name in enumerate(users):
+    users_B[index] = name
+
+print(users_B)
+
+# 3
+users_C = (sorted(users_A.items()))
+
+print(users_C)
+
