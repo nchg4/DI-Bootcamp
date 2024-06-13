7f8e2ab92c3c1023d3c35b029796a0ee852e8e7a
nchg4 <noamglick@yahoo.com>
2024-06-14 00:23:39 +0300

    daily challenge

---
+++ Week 2/Day 4/Daily Challenge/dailychallenge.py
@@ -0,0 +1,26 @@
+# Matrix
+# Multi demensinal list 
+# an array w another array inside of it 
+
+matrix = [
+   ['7', 'i', 'i'],
+   ['T', 's', 'x'],
+   ['h', '%', '?'],
+   ['i', ' ', '#'],
+   ['s', 'M', ' '],
+   ['$', 'a', ' '],
+   ['#', 't', '%'],
+   ['^', 'r', '!'],
+]
+
+print(matrix[1][0],matrix[2][0],matrix[3][0],matrix[4][0],
+       matrix[3][1], matrix[0][1],matrix[1][1],matrix[3][1],
+       matrix[4][1],matrix[5][1],matrix[6][1],matrix[7][1],matrix[0][2],matrix[1][2]
+       )
+
+
+
+
+
+
+
