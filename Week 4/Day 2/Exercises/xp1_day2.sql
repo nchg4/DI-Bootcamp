5707ef2c54c96899394987437c18ed8403c526d2
nchg4 <noamglick@yahoo.com>
2024-06-24 11:43:20 +0300

    commit xp1

---
+++ Week 4/Day 2/Exercises/xp1_day2.sql
@@ -0,0 +1,21 @@
+-- SELECT * FROM public.items_list
+-- ORDER BY id ASC 
+
+-- SELECT * FROM public.items_list
+-- ORDER BY price;
+
+-- SELECT * FROM public.items_list
+-- where price >= 80
+-- ORDER BY price  DESC;
+
+
+-- SELECT * FROM public.customers_table
+-- ORDER BY id ASC 
+	
+-- SELECT "First Name", "Last Name" FROM public.customers_table
+-- order by "First Name" asc
+-- limit 3; 
+
+
+-- SELECT "Last Name" FROM public.customers_table
+-- ORDER BY "Last Name" DESC;
