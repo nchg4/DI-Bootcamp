03415b1e1e579b8301587d9055f14e88216a1ec4
Noam Glick <noamglick@Noams-MacBook-Pro.local>
2024-07-14 16:54:14 +0300

    commited daily

--- Week 7/Day 1/Daily Challenge/script2.js
+++ Week 7/Day 1/Daily Challenge/script2.js
@@ -45,44 +45,59 @@ const morse = `{
     "@": ".--.-.",
     "(": "-.--.",
     ")": "-.--.-"
-  }`
+}`;
 
-function toJs(){
-    const morseObject = JSON.parse(morse);
-    // console.log("morseObject:", morseObject);
+function toJs() {
     return new Promise((resolve, reject) => {
-        if (morse.length === 0){
-            reject("Morse string is empty")
-        }else{
+        if (morse.length === 0) {
+            reject("Morse string is empty");
+        } else {
             const morseObject = JSON.parse(morse);
             resolve(morseObject);
         }
     });
 }
 
-function isWordOk(word, possibleLetters){
-    const userLetters = word.split("")
-    return userLetters.every((letter) => !possibleLetters.includes
-    (letter))
+function isWordOk(word, possibleLetters) {
+    const userLetters = word.split("");
+    return userLetters.every((letter) => possibleLetters.includes(letter));
 }
 
 function toMorse(morseJS) {
-    const word = prompt("Enter a word:");
+    const possibleLetters = Object.keys(morseJS);
+    const word = prompt("Enter a word:").toLowerCase();
     return new Promise((resolve, reject) => {
-        
+        if (!isWordOk(word, possibleLetters)) {
+            reject("Word contains invalid characters.");
+        } else {
+            const result = { word: word, morse: convertWordToMorse(word, morseJS) };
+            resolve(result);
+        }
     });
 }
 
-function convertWordToMorse(word, morseJS){
-    const letters = word.split("")
-    for (const letter of letters){
-        console.log(morseJS[letter]);
-    }
+function convertWordToMorse(word, morseJS) {
+    const letters = word.split("");
+    return letters.map(letter => morseJS[letter]).join(" "); // Join the Morse codes with a space
+}
+
+function joinWords(obj){
+    const word = obj.word;
+    console.log("word:", word);
+    const morse = obj.morse
+    console.log("morse:", morse);
+    document.getElementById("word").innerText = word;
+    const morseArray = morse.split(" ");
+    morseArray.forEach((sign, i) => {
+        const p = document.createElement("p");
+        p.innerText = word[i] + " = " + sign;
+        document.getElementById ("morse").appendChild(p);
+    });
+   
+       
 }
 
 toJs()
-.then((morseObj) => toMorse(morseObj))
-.then((res) => console.log(res))
-.catch(err => console.error(err));
-
-
+    .then((morseObj) => toMorse(morseObj))
+    .then((wordAndTranslation) => joinWords(wordAndTranslation))
+    .catch((err) => console.error(err));
