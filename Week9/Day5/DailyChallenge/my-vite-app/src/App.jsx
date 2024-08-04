e55312a3a5ba8ab8ac5c2da2e9d3e9df490e6458
Noam <noamglick@yahoo.com>
2024-08-04 19:23:06 +0300

    commit

--- Week9/Day5/DailyChallenge/my-vite-app/src/App.jsx
+++ Week9/Day5/DailyChallenge/my-vite-app/src/App.jsx
@@ -1,34 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num1, setNum1] = useState('')
+  const [num2, setNum2] = useState('')
+  const [result, setResult] = useState(null)
+  const [operation, setOperation] = useState('+')
+
+  const calculateResult = () => {
+    const n1 = parseFloat(num1)
+    const n2 = parseFloat(num2)
+
+    switch (operation) {
+      case '+':
+        setResult(n1 + n2)
+        break
+      case '-':
+        setResult(n1 - n2)
+        break
+      case '*':
+        setResult(n1 * n2)
+        break
+      case '/':
+        setResult(n2 !== 0 ? n1 / n2 : 'Not divisible by zero')
+        break
+      default:
+        setResult('Invalid operation')
+    }
+  }
+
+  const clearAll = () => {
+    setNum1('')
+    setNum2('')
+    setResult(null)
+    setOperation('+')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="calculator">
+      <h1> Calculator </h1>
+      <input
+        id="num1"
+        type="number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <label htmlFor="operation">Operation:</label>
+      <select id="operation" value={operation} onChange={(e) => setOperation(e.target.value)}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">x</option>
+        <option value="/">÷</option>
+      </select>
+      <input
+        id="num2"
+        type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <div className="button-group">
+        <button onClick={calculateResult}>Calculate</button>
+        <button onClick={clearAll} className="clear-button">AC</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      {result !== null && (
+        <p className={parseFloat(result) < 0 ? 'result-negative' : 'result-positive'}>
+          Result: {result}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      )}
+    </div>
   )
 }
 
